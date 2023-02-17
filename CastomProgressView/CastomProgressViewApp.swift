2fc2ae76e99084b77d9535a9157fd7c1bedab868
projec1707 <pwroma@gmail.com>
2023-02-17 14:36:50 +0200

    Initial Commit

---
+++ CastomProgressView/CastomProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CastomProgressViewApp.swift
+//  CastomProgressView
+//
+//  Created by Roman Riepa on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CastomProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
